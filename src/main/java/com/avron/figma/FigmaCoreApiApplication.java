cd07453a7814b80103fe1b7eb0816be0554ad6f6
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-06-05 16:47:23 +0530

    commit with all the API contract developments

---
+++ src/main/java/com/avron/figma/FigmaCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.avron.figma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigmaCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FigmaCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avron/figma/FigmaCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avron.figma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigmaCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
